819acb308aa12ef15178e72212cbac7963b0c57c
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-07-08 18:55:40 +0200

    add dest pos

--- src/main.js
+++ src/main.js
@@ -6,7 +6,23 @@ const k = window.k = kaplay({ background: '#006994' })
 
 k.loadSpriteAtlas('sprites/ships_sheet.png', data)
 
-k.add([
-  k.pos(120, 80),
-  k.sprite('ship (1)')
-])
+k.scene('main', () => {
+  const destPos = new k.Vec2(k.width() / 2, k.height() / 2)
+
+  const ship = k.add([
+    k.pos(destPos),
+    k.sprite('ship (1)'),
+    k.area(),
+    k.rotate()
+  ])
+
+  ship.onUpdate(() => {
+    ship.angle = ship.pos.angle(destPos) + 90
+  })
+
+  k.onMousePress(() => {
+    Object.assign(destPos, k.mousePos())
+  })
+})
+
+k.go('main')
