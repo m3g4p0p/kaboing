b72c1b5b5ac7c1f3b6685e41c4da67f787f6a9f6
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-07-09 06:04:52 +0200

    add destpos component

--- src/main.js
+++ src/main.js
@@ -6,26 +6,41 @@ const k = window.k = kaplay({ background: '#006994' })
 
 k.loadSpriteAtlas('sprites/ships_sheet.png', data)
 
-k.scene('main', () => {
-  const destPos = new k.Vec2(k.width() / 2, k.height() / 2)
+/**
+ * @param {k.Vec2} pos
+ */
+function destPos (pos) {
+  return {
+    id: 'destpos',
+    require: ['pos', 'rotate'],
 
+    update () {
+      const dt = k.dt()
+      const destAngle = this.pos.angle(pos) + 90
+      const delta = destAngle - this.angle
+      const distance = pos.sub(this.pos)
+
+      this.pos = this.pos.add(distance.scale(dt))
+      this.angle += delta * dt
+    },
+
+    destroy () {
+      console.log(this)
+    }
+  }
+}
+
+k.scene('main', () => {
   const ship = k.add([
-    k.pos(destPos),
+    k.pos(k.width() / 2, k.height() / 2),
     k.sprite('ship (1)'),
     k.area(),
     k.rotate(),
     k.anchor('center')
   ])
 
-  ship.onUpdate(() => {
-    const destAngle = ship.pos.angle(destPos) + 90
-    const delta = destAngle - ship.angle
-
-    ship.angle += delta * k.dt()
-  })
-
   k.onMousePress(() => {
-    Object.assign(destPos, k.mousePos())
+    ship.use(destPos(k.mousePos()))
   })
 })
 
