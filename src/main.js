a43ea720c4bd0fc2db38e4d3e70da5298c5fbd9f
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-07-12 20:51:25 +0200

    add player collision

--- src/main.js
+++ src/main.js
@@ -3,43 +3,46 @@ import { k } from './setup'
 import { spawnShip } from './spawn'
 
 k.scene('start', () => {
+  k.add([k.text('Say arrrrr to start')])
   k.onMousePress(() => k.go('main'))
 })
 
 k.scene('main', () => {
-  k.layers(['below', 'above'], 'above')
-
   const player = spawnShip([
     k.pos(k.width() / 2, k.height() / 2),
     k.sprite('ship (1)'),
     'player'
   ])
 
+  player.onDestroy(() => {
+    k.go('start')
+  })
+
   k.onMousePress(() => {
     player.use(towards(k.mousePos()))
-    console.log(player.layer)
+  })
+
+  k.onCollide('ship', 'ship', (a, b) => {
+    k.shake(30)
+
+    for (const current of [a, b]) {
+      current.unuse('ship')
+      current.use(vanish())
+      current.use(k.layer('below'))
+    }
   })
 
   k.loop(3, () => {
     const enemy = spawnShip([
       k.pos(k.rand(k.vec2(k.width(), k.height()))),
       k.sprite('ship (2)'),
+      k.offscreen({ destroy: true }),
       'enemy'
     ])
 
     enemy.onUpdate(() => {
       if (!enemy.is(['sink', 'towards'])) {
         enemy.use(towards(player.pos))
-      }
-    })
-
-    enemy.onCollide('enemy', other => {
-      k.shake(30)
-
-      for (const current of [enemy, other]) {
-        current.unuse('enemy')
-        current.use(vanish())
-        current.use(k.layer('below'))
       }
     })
   })
--- src/setup.js
+++ src/setup.js
@@ -2,6 +2,10 @@ import kaplay from 'kaplay'
 import { loadAtlasData } from './util'
 
 const data = await loadAtlasData('sprites/ships_sheet.xml')
-export const k = window.k = kaplay({ background: '#006994' })
 
+export const k = window.k = kaplay({
+  background: '#006994'
+})
+
+k.layers(['below', 'above'], 'above')
 k.loadSpriteAtlas('sprites/ships_sheet.png', data)
