4311261efe8be0672a680384c31e2081e471c472
Sebastian Seitz <bassistsagainstracists@gmx.net>
2024-07-12 17:27:37 +0200

    add enemies

--- src/main.js
+++ src/main.js
@@ -4,20 +4,39 @@ import { k } from './setup'
 function spawn (components) {
   return k.add([
     k.area(),
-    k.rotate(),
+    k.rotate(k.rand(360)),
     k.anchor('center'),
     ...components
   ])
 }
 
 k.scene('main', () => {
-  const ship = spawn([
+  const player = spawn([
     k.pos(k.width() / 2, k.height() / 2),
-    k.sprite('ship (1)')
+    k.sprite('ship (1)'),
+    'player'
   ])
 
   k.onMousePress(() => {
-    ship.use(towards(k.mousePos()))
+    player.use(towards(k.mousePos()))
+  })
+
+  k.loop(3, () => {
+    if (k.get('enemy').length > 2) {
+      return
+    }
+
+    const enemy = spawn([
+      k.pos(k.rand(k.vec2(k.width(), k.height()))),
+      k.sprite('ship (2)'),
+      'enemy'
+    ])
+
+    enemy.onUpdate(() => {
+      if (!enemy.is('towards')) {
+        enemy.use(towards(player.pos))
+      }
+    })
   })
 })
 
